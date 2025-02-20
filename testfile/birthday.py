9d5a60c21b25ddd5a096a4a850898bc74eb1595f
BBiT808 <witchsparadise11003311@gmail.com>
2025-02-20 13:54:11 +0900

    2025.02.20

---
+++ testfile/birthday.py
@@ -0,0 +1,19 @@
+import datetime
+today = datetime.datetime.now().date()
+
+def mom():
+    mommybirthday = today
+    import time as t
+    
+    if mommybirthday == (datetime.datetime.now().date()):
+        print("💞사랑하는💞")
+        t.sleep(1)
+        print("💝우리 엄마의💝")
+        t.sleep(1)
+        print("🥳🎉생신을 축하합니다!!🥳🎉")
+        t.sleep(1)
+        print("""        ＊~iiii~＊
+        ∴┍★☆┑∴
+      ∵┍*HAPPY-*┑∵
+      ┍♡BIRTHDAY♡┑
+      ┖☆:*~**~*:☆┚""")
